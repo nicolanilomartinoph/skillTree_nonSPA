6ae92eff9a35c5809d84af9fcc34dc5dd839b3da
Nicola Nilo Z. Martino <nicolanilomartinoph@gmail.com>
2020-04-28 12:16:58 +0800

    gen dat agetter 2

--- public/js/app.js
+++ public/js/app.js
@@ -1961,6 +1961,8 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _SkillLevel_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./SkillLevel.vue */ "./resources/js/components/skills/SkillLevel.vue");
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
 //
 //
 //
@@ -1984,52 +1986,127 @@ __webpack_require__.r(__webpack_exports__);
   data: function data() {
     return {
       tree_data: {
-        skills: this.skills_details,
-        depth: 0,
-        level_data: []
+        limbs: this.skills_details,
+        // Limbs refer to the base skills
+        gens_data: []
       }
     };
   },
+  // Here we will use the concept of a family tree for the variables
   methods: {
-    get_level_data: function get_level_data() {
-      var skills = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.tree_data.skills;
-      this.tree_data.depth++;
-      var level_data = {
-        branch_count: skills.length,
-        branch_leaf_width: "test"
-      };
-      this.tree_data.level_data.push(level_data); // is the width of the level but each node may vary in width
-
-      this.get_base_skill_data();
-    },
-    get_base_skill_data: function get_base_skill_data() {
+    get_tree_generations_data2: function get_tree_generations_data2(skills) {
       var _this = this;
 
-      var skills = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.tree_data.skills;
-      skills.forEach(function (element) {
-        if (element.parent_skills == "base") {
-          element.node_depth = 0; //element.node_max_width = 0;
+      var parent = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+      var ancestor = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+      var isLimb = false; // BASE OR NOT ???
 
-          _this.scan_depth(element, element.child_skills);
+      skills.forEach(function (skill) {
+        // IF BASE
+        if (skill.parent_skills[0] === "base") {
+          skill.limb_data = [], skill.gen_nth = 1;
+          console.log("BASE");
+          isLimb = (_readOnlyError("isLimb"), true);
+        } // IF NOT BASE
+        else {
+            skill.branch_data = {
+              gen_nth: parent.gen_nth + 1,
+              width: parent.child_skills.length
+            }; // Send data to ancestor
+
+            ancestor.limb_data.push(skill.branch_data);
+          }
+      }); // Recurse
+
+      skills.forEach(function (skill) {
+        if (skill.gen_nth === 1) {
+          if (skill.child_skills) {
+            skill.child_skills.forEach(function (element) {
+              _this.get_tree_generations_data2(new Array(element), skill, skill);
+            });
+          }
+        } else {
+          if (skill.child_skills) {
+            skill.child_skills.forEach(function (element) {
+              _this.get_tree_generations_data2(new Array(element), skill, ancestor);
+            });
+          }
         }
       });
-    },
-    scan_depth: function scan_depth(base_skill) {
-      var _this2 = this;
+      /**
+       * CORRECT THE limb_data
+       * accumulate how many obj have the same gen_nth = max level width
+       * reduce it, push to limb_data
+       * 
+       * depth = limb_data.length
+       */
 
-      var child_skills = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-
-      if (child_skills) {
-        base_skill.node_depth++; // base_skill.node_max_width = base_skill.node_max_width > base_skill.child_skills.length ? base_skill.node_max_width : base_skill.child_skills.length;
-
-        child_skills.forEach(function (element) {
-          _this2.scan_depth(base_skill, element.child_skills);
+      if (isLimb) {
+        skills.forEach(function (skill) {
+          skill.limb_data.forEach;
         });
       }
+    },
+    get_tree_generations_data: function get_tree_generations_data(skills) {
+      var _this2 = this;
+
+      var parent = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+      // Current Gen getter
+      function apply_current_gen_nth() {
+        if (parent === null) {
+          return 1;
+        } else {
+          return parent.gen_nth + 1;
+        }
+      } // Tag each skill with .gen_nth in current generation
+
+
+      skills.forEach(function (skill) {
+        if (!skill.hasOwnProperty("gen_nth")) {
+          skill.gen_nth = apply_current_gen_nth();
+        }
+      });
+
+      function get_our_gens_data_object(skill, gens_data) {
+        if (!gens_data.length) {
+          return null;
+        } else {
+          for (var i = 0; i < gens_data.length; i++) {
+            if (gens_data[i].gens_nth === skill.gen_nth) {
+              return gens_data[i];
+            }
+          }
+
+          return null;
+        }
+      } // Let's send a collective data of our current gen
+
+
+      skills.forEach(function (skill) {
+        var our_gens_data_object = get_our_gens_data_object(skill, _this2.tree_data.gens_data);
+
+        if (our_gens_data_object === null) {
+          _this2.tree_data.gens_data.push({
+            gens_nth: apply_current_gen_nth(),
+            gens_siblings: [skill.skill_name]
+          });
+        } else {
+          our_gens_data_object.gens_siblings.push(skill.skill_name);
+        }
+      }); // Recurse for each child of the current generation
+
+      skills.forEach(function (skill) {
+        if (skill.child_skills.length > 0) {
+          skill.child_skills.forEach(function (element) {
+            _this2.get_tree_generations_data(new Array(element), skill);
+          });
+        }
+      });
     }
   },
   mounted: function mounted() {
-    this.get_level_data();
+    this.get_tree_generations_data2(this.tree_data.limbs);
   },
   components: {
     'skill-level': _SkillLevel_vue__WEBPACK_IMPORTED_MODULE_0__["default"]
--- resources/js/components/skills/SkillTree.vue
+++ resources/js/components/skills/SkillTree.vue
@@ -20,54 +20,128 @@ import skillLevel from './SkillLevel.vue'
 
 export default {
     props: ['skills_details'],
-    data() {
+    data: function () {
         return {
             tree_data: {
-                skills: this.skills_details,
-                depth: 0,
-                level_data: []
-            }
-        }
+                limbs: this.skills_details, // Limbs refer to the base skills
+                gens_data: [],
+            },
+        }    
     },
+    // Here we will use the concept of a family tree for the variables
     methods: {
-        get_level_data: function(skills = this.tree_data.skills) 
+        get_tree_generations_data2: function(skills, parent = null, ancestor = null)
         {
-            this.tree_data.depth++;
-            let level_data = {
-                branch_count : skills.length,
-                branch_leaf_width : "test"
-            }
-            this.tree_data.level_data.push(level_data); // is the width of the level but each node may vary in width
-
-            this.get_base_skill_data();
-        },
-        get_base_skill_data: function(skills = this.tree_data.skills) 
-        {
-            skills.forEach(element => {
-                if(element.parent_skills == "base")
-                {
-                    element.node_depth = 0;
-                    //element.node_max_width = 0;
-                    this.scan_depth(element, element.child_skills);
+            const isLimb = false
+            // BASE OR NOT ???
+            skills.forEach(skill => {
+                // IF BASE
+                if(skill.parent_skills[0] === "base") {
+                    skill.limb_data = [],
+                    skill.gen_nth = 1;
+                    console.log("BASE")
+                    isLimb = true
                 }
-            });    
-        },
-        scan_depth: function(base_skill, child_skills = null)
-        {
-            if(child_skills) 
-            {
-                base_skill.node_depth++;
-                // base_skill.node_max_width = base_skill.node_max_width > base_skill.child_skills.length ? base_skill.node_max_width : base_skill.child_skills.length;
+                // IF NOT BASE
+                else{
+                    skill.branch_data = {
+                        gen_nth: parent.gen_nth + 1,
+                        width: parent.child_skills.length
+                    }
 
-                child_skills.forEach(element => {
-                    this.scan_depth(base_skill, element.child_skills);
+                    // Send data to ancestor
+                    ancestor.limb_data.push(skill.branch_data)
+                }
+            })
+
+            // Recurse
+            skills.forEach(skill => {
+                if(skill.gen_nth === 1){
+                    if(skill.child_skills) {
+                        skill.child_skills.forEach(element => {
+                            this.get_tree_generations_data2(new Array(element), skill, skill)
+                        })
+                    }
+                }else{
+                    if(skill.child_skills) {
+                        skill.child_skills.forEach(element => {
+                            this.get_tree_generations_data2(new Array(element), skill, ancestor)
+                        })
+                    }
+                }
+            })
+
+            /**
+             * CORRECT THE limb_data
+             * accumulate how many obj have the same gen_nth = max level width
+             * reduce it, push to limb_data
+             * 
+             * depth = limb_data.length
+             */
+            if(isLimb){
+                skills.forEach(skill => {
+                    skill.limb_data.forEach
                 })
             }
-        }
+        },
+        get_tree_generations_data: function(skills, parent = null) 
+        {
+            // Current Gen getter
+            function apply_current_gen_nth() {
+                if(parent === null) {
+                    return 1
+                }else{
+                    return parent.gen_nth + 1
+                }
+            }
+
+            // Tag each skill with .gen_nth in current generation
+            skills.forEach(skill => {
+                if(!skill.hasOwnProperty("gen_nth")) {
+                    skill.gen_nth = apply_current_gen_nth()
+                }
+            })
+            
+            function get_our_gens_data_object (skill, gens_data)  {
+                if(!gens_data.length){
+                    return null
+                }else{
+                    for(let i = 0; i < gens_data.length; i++) {
+                        if(gens_data[i].gens_nth === skill.gen_nth) {
+                            return gens_data[i]
+                        }
+                    }
+                    return null
+                }
+            }
+
+            // Let's send a collective data of our current gen
+            skills.forEach(skill => {
+                const our_gens_data_object = get_our_gens_data_object(skill, this.tree_data.gens_data)
+
+                if(our_gens_data_object === null) {
+                    this.tree_data.gens_data.push({
+                        gens_nth: apply_current_gen_nth(),
+                        gens_siblings: [skill.skill_name]
+                    });
+                }else{
+                    our_gens_data_object.gens_siblings.push(skill.skill_name)
+                }
+            })
+            
+            // Recurse for each child of the current generation
+            skills.forEach(skill => {
+                if(skill.child_skills.length > 0) {
+                    skill.child_skills.forEach(element => {
+                        this.get_tree_generations_data(new Array(element), skill)
+                    })
+                }
+            })
+        }, 
     }, 
     mounted: function()
     {
-        this.get_level_data();
+        this.get_tree_generations_data2(this.tree_data.limbs);
     },
     components: {
         'skill-level': skillLevel,
